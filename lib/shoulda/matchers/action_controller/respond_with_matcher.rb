e61d81365b36a9a3aebaec506d7bd2112cf4d583
Tomáš Kuča <tomas.kuca@matfyz.cz>
2012-09-09 22:04:27 +0200

    Explicitly pass controller to respond_with matcher

--- lib/shoulda/matchers/action_controller/respond_with_matcher.rb
+++ lib/shoulda/matchers/action_controller/respond_with_matcher.rb
@@ -16,17 +16,17 @@ module Shoulda # :nodoc:
       #   it { should respond_with(:error)    }
       #   it { should respond_with(501)       }
       def respond_with(status)
-        RespondWithMatcher.new(status)
+        RespondWithMatcher.new(self, status)
       end
 
       class RespondWithMatcher # :nodoc:
 
-        def initialize(status)
+        def initialize(controller, status)
+          @controller = controller
           @status = symbol_to_status_code(status)
         end
 
-        def matches?(controller)
-          @controller = controller
+        def matches?(_)
           correct_status_code? || correct_status_code_range?
         end
 
